605aa21601c46e9a9e8b888034c8f2d6527b369e
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-11-11 22:29:59 -0500

    inicio

---
+++ src/main/java/com/centroinformacion/Sw2Java17ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.centroinformacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sw2Java17ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sw2Java17ApiGatewayApplication.class, args);
+	}
+
+}
